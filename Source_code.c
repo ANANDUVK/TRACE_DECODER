5a6a04f3f34c83f8d20f2aa0b4188ab88f4e82a8
ANANDU V K <96731751+ANANDUVK@users.noreply.github.com>
2025-10-22 01:26:08 +0530

    Update Source_code.c
    Previous code only parse and identified  fixed length field values, In this code every field values of a SYNC message is identified and printed .
    
    To Do : Create seperate a source file containing functions definitions, code logic, etc. and its header file with functions declarations.

--- Source_code.c
+++ Source_code.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-int main() {
+int parse_function(const char *filename){
     FILE *fp = fopen("ntracedata.rtd", "rb");  // Open file in binary mode
     if (!fp) {
         printf("Error: Could not open trace file.\n");
@@ -14,7 +14,7 @@ int main() {
     long filesize = ftell(fp);
     fseek(fp, 0, SEEK_SET);
 
-    printf("Opened trace file, size: %ld bytes\n", filesize);
+    printf("Opened trace file  '%s', size: %ld bytes\n\n",filename, filesize);
 
     // Allocate buffer and read file
     uint8_t *buffer = malloc(filesize);
@@ -25,22 +25,26 @@ int main() {
     }
 
     size_t read = fread(buffer, 1, filesize, fp);
-    printf("Read %zu bytes from trace file\n", read);
+    printf("Read %zu bytes from trace file\n\n\n\n", read);
 
     fclose(fp);
 
-    // Now buffer contains the trace data, ready for parsing SYNC messages!  
+    // Now buffer contains the trace data, ready for parsing SYNC messages
 
      // Parsing N-Trace file stream byte-by-byte
     
      long i=0;
-     int flag=0;
-     long message_start=0;
-     long message_end=0;
-     int sync_count=0;
-     unsigned __int128 bitstream = 0;
-     int bitpos = 0;
+     int flag=0;    // Flag used to 
+     long message_start=0;  // variable that hold  start address of  each SYNC message
+     long message_end=0;    // variable that hold  end address of  each SYNC message
+     int sync_count=0;  // Variable used to count no. of SYNC messages in the provided Trace file
 
+     unsigned __int128 bitstream = 0;   // Bit stream used to store MDOs of each SYNC messages for parsing
+
+     int bitpos = 0;     // 
+     int icnt_end_pos = 0;
+     int msg_end_pos = 0;
+    //  int icnt_done = 0;
 
      while(i < filesize){
 
@@ -52,44 +56,62 @@ int main() {
         // Detect start of any SYNC message in the data stream
         if(MDO==0x09 && MSEO==0){
             message_start=i;
-            flag=1;
+            flag=1;    // flagged to indicate SYNC message started
             sync_count++;
             bitstream=0;
             bitpos=0;
+            icnt_end_pos=0;  // Reset Instruction_counter end position for every SYNC message start
             printf("SYNC message start at offset 0x%04lx\n",message_start);
 
-            //appending first chunk 
-            bitstream |= ((unsigned __int128)MDO) << bitpos;
-            bitpos += 6;
+            // //appending first chunk 
+            // bitstream |= ((unsigned __int128)MDO) << bitpos;
+            // bitpos += 6;
         }
         
         
         //  Combining all 6-bit chunks (MDO of each Bytes of a SYNC message) into one continous bitstream of 64 buts
-        else if(flag){
+        if(flag){
              // append 6 bits of MDO, LSB-first
             bitstream |= ((unsigned __int128)MDO) << bitpos;
             bitpos += 6;
-        }
+            if(MSEO==0x01 && icnt_end_pos==0){  // (Safety) icnt_end_pos checked to avoid multiple assigning bitpos.
+                icnt_end_pos=bitpos;   //  Bit position when variable length field ends
                 
+            }
+        }
+
+        
+
         // Detect  end of curent SYNC message
-        if(flag && MSEO==3){
+        if(flag && MSEO==0x03){
             message_end=i;
             printf("SYNC message end at offset 0x%04lx\n",message_end);
             printf("Length is %ld\n",message_end- message_start  +1);
             
+            
+            msg_end_pos=bitpos; //  Message end bit position 
+
+            // Safety: if no 0x01 found in SYNC message , then I_CNT ends before message end
+            if (icnt_end_pos == 0) {
+                icnt_end_pos = msg_end_pos;
+                                    }
 
             //extract fixed fields
             uint8_t TCODE = bitstream & 0x3F;       // first 6 bits of each SYNC message
-            uint8_t SRC   = (bitstream >> 6) & 0x0F;
-            uint8_t SYNC  = (bitstream >> 10) & 0x0F;
-            uint8_t BTYPE = (bitstream >> 14) & 0x03;
+            uint8_t SRC   = (bitstream >> 6) & 0x0F;   // SRC include bits from 7th position to 10th position (4 bits)
+            uint8_t SYNC  = (bitstream >> 10) & 0x0F;  // SYNC includes bits from 11th position to 14th position (4 bits)
+            uint8_t BTYPE = (bitstream >> 14) & 0x03;   // BTYPE includes bits from 15th position to 16th position (2 bits)
+
+            // Exctract variable length fields
+            unsigned long long I_CNT = (unsigned long long)((bitstream >> 16) & (((__int128)1 << (icnt_end_pos - 16)) - 1));
+            unsigned long long F_ADDR =  (unsigned long long)((bitstream >> icnt_end_pos) & (((__int128)1 << (msg_end_pos - icnt_end_pos)) - 1));
 
             printf("Decoded fields of SYNC message %d is :\n", sync_count);
-            printf("TCODE:0x%0X\n SRC:0x%0X\n SYNC:0x%0X\n BTYPE:0x%0X\n\n",TCODE, SRC, SYNC, BTYPE);
+            printf("TCODE:0x%02X\n SRC:0x%0X\n SYNC:0x%0X\n BTYPE:0x%0X\n  I_COUNT:0x%llX\n  F_ADDR:0x%llX\n\n",TCODE, SRC, SYNC, BTYPE, I_CNT,F_ADDR);
             
-            flag=0;
-            bitstream=0;
-            bitpos=0;
+            flag=0;    // Flagged to indicate SYNC messsage end
+            bitstream=0;  // Reinitialize bitstream for next SYNC message
+            bitpos=0;     // Reinitialize bitposition for next SYNC message
         }
 
         i++;
@@ -97,5 +119,16 @@ int main() {
      }
  printf("Total no. of SYNC messages in the file streams are : %d\n",sync_count);
  free(buffer);
- return 0;
+}
+
+
+int main(){
+    printf("============================================\n");
+    printf("    RISC-V N-TRACE SYNC Message Decoder\n  ");
+    printf("===============================================\n");
+    parse_function("ntracedata.rtd");
+    printf("\nDecoding complete\n");
+    printf("============================================\n");
+    return 0;
+
 }
